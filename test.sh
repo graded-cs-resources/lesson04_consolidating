68878db416baf388dfe8c2980c12f024f7299534
David Griswold <david.griswold@graded.br>
2019-09-15 22:52:16 -0300

    still testing

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ git checkout comments
 git push -u origin comments
 git checkout master
 d="DavidGriswoldTeacher"
-hub pull-request -d -r ${d} -b comments -h master -m "Comments Thread" \
+hub pull-request -d -b comments -h master -m "Comments Thread" \
     -m "Your page is published at https://graded-ib-cs-hl-2019-2020.github.io/summative-assignment-1-${d}." \
     -m "I will put comments and (eventually) a grade for your project on this page - you should get notifications!" \
     -m "Please do NOT push the \"Merge Pull Request\" button, as it will make these comments disappear!"
